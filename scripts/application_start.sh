5b3b9498f7ab7b936cb76830f65f47ec92e4a338
Rohini-Jadhav <rjrohini.jadhav@gmail.com>
2021-11-30 17:19:31 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/freelance
+sudo chmod -R 777 /home/ec2-user/node_app
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/freelance
+cd /home/ec2-user/node_app
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
